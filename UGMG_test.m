07dcd653c18afe484473cfd4a8c9470c25ff1800
Oleksandr Frei <oleksandr.frei@gmail.com>
2017-11-15 13:43:51 +0100

    Include BMI, HEIGHT and SCZ3 in UGMG_test

--- UGMG_test.m
+++ UGMG_test.m
@@ -2,7 +2,7 @@ addpath('DERIVESTsuite');
 addpath('H:\NORSTORE\oleksanf\holland_genetics\GPSIM');  % for GenStats_meta
 addpath('H:\NORSTORE\oleksanf\holland_matlab\qq');
 
-workspace_data_file = 'data_2017_11_10.mat';
+workspace_data_file = 'data_2017_11_15.mat';
 if 0
     % Test consistency with Dominic's model
     info1m = load('H:\NORSTORE\MMIL\SUMSTAT\LDSR\MATLAB_Annot\infomat.mat');
@@ -73,6 +73,10 @@ if 0
     bip_noMETA = load('H:\NORSTORE\MMIL\new_9m_SNPs_processed_summary\GWAS_Data\PGC_BIP32_9m.mat');
     bip.zvec = -norminv(10.^-bip_noMETA.logpvec_pgc_bip32/2).*sign(bip_noMETA.zvec_pgc_bip32);
     bip.zvec(~isfinite(bip.zvec_meta)) = nan;
+    
+    bmi    = load('H:\NORSTORE\MMIL\SUMSTAT\MAT_9M\GIANT_BMI_2015_EUR_lift.mat');
+    height = load('H:\NORSTORE\MMIL\SUMSTAT\MAT_9M\GIANT_HEIGHT_2014_lift.mat');
+    scz3   = load('H:\NORSTORE\MMIL\SUMSTAT\MAT_9M\PGC_SCZ_0917.mat');
 
     % Align Alex's reference data
     if 0
@@ -114,6 +118,10 @@ if 0
     bip.defvec_1M = defvec0 & mask_in_9m & isfinite(bip.zvec); fprintf('sum(bip.defvec_1M) = %i\n', sum(bip.defvec_1M));
     scz.defvec_5M = defvec0 &              isfinite(scz.zvec); fprintf('sum(scz.defvec_5M) = %i\n', sum(scz.defvec_5M));
     bip.defvec_5M = defvec0 &              isfinite(bip.zvec); fprintf('sum(bip.defvec_5M) = %i\n', sum(bip.defvec_5M));
+    
+    bmi.defvec_1M    = defvec0 & mask_in_9m & isfinite(bmi.zvec);    fprintf('sum(bmi.defvec_1M) = %i\n',    sum(bmi.defvec_1M));
+    height.defvec_1M = defvec0 & mask_in_9m & isfinite(height.zvec); fprintf('sum(height.defvec_1M) = %i\n', sum(height.defvec_1M));    
+    scz3.defvec_1M = defvec0 & mask_in_9m & isfinite(scz3.zvec); fprintf('sum(height.defvec_1M) = %i\n', sum(scz3.defvec_1M));    
 
     if any(scz.defvec_1M ~= defvecForAlex1m_new.defvec), warning('scz.defvec_1M mismatch'); end;
     if any(scz.defvec_5M ~= defvecForAlexScz5m.defvec), warning('scz.defvec_5M mismatch'); end;
@@ -133,8 +141,15 @@ if 0
     bip.pruneidxmat_5M = false(size(bip.defvec_5M,1), nprune);
     for repi=1:nprune, tmp=rand(size(chrnumvec,1),1); tmp(~bip.defvec_5M) = NaN; bip.pruneidxmat_5M(:,repi) = isfinite(FastPrune(tmp, LDmat)); fprintf('.'); end; fprintf('\n');
 
+    bmi.pruneidxmat_1M = false(size(bmi.defvec_1M,1), nprune);
+    for repi=1:nprune, tmp=rand(size(chrnumvec,1),1); tmp(~bmi.defvec_1M) = NaN; bmi.pruneidxmat_1M(:,repi) = isfinite(FastPrune(tmp, LDmat)); fprintf('.'); end; fprintf('\n');
+    height.pruneidxmat_1M = false(size(height.defvec_1M,1), nprune);
+    for repi=1:nprune, tmp=rand(size(chrnumvec,1),1); tmp(~height.defvec_1M) = NaN; height.pruneidxmat_1M(:,repi) = isfinite(FastPrune(tmp, LDmat)); fprintf('.'); end; fprintf('\n');
+    scz3.pruneidxmat_1M = false(size(scz3.defvec_1M,1), nprune);
+    for repi=1:nprune, tmp=rand(size(chrnumvec,1),1); tmp(~scz3.defvec_1M) = NaN; scz3.pruneidxmat_1M(:,repi) = isfinite(FastPrune(tmp, LDmat)); fprintf('.'); end; fprintf('\n');
+
     %save(data_file, 'scz', 'bip', 'alex',                     'chrnumvec', 'posvec', 'mask_in_9m', 'index_to_9m', 'mafvec_9m', 'Hvec_9m', 'tldvec_9m', 'numSNPsInLDr2_gt_r2min_vec');
-    save(workspace_data_file, 'scz', 'bip',          'alex_shape_param', 'chrnumvec', 'posvec', 'mask_in_9m', 'index_to_9m', 'mafvec_9m', 'Hvec_9m', 'tldvec_9m', 'numSNPsInLDr2_gt_r2min_vec', 'nprune');
+    save(workspace_data_file, 'scz', 'bip', 'bmi', 'height', 'scz3',  'alex_shape_param', 'chrnumvec', 'posvec', 'mask_in_9m', 'index_to_9m', 'mafvec_9m', 'Hvec_9m', 'tldvec_9m', 'numSNPsInLDr2_gt_r2min_vec', 'nprune');
     keyboard
     clear
     return
@@ -145,10 +160,16 @@ else
 end
 
 
+
+
 %task=scz; disp_trait1_name='SCZ'; zvec=task.zvec; zvec(~task.defvec_1M) = nan; nvec=task.Neff; pruneidxmat=task.pruneidxmat_1M;clear('task');
 %task=scz; disp_trait1_name='SCZ'; zvec=task.zvec; zvec(~task.defvec_5M) = nan; nvec=task.Neff; pruneidxmat=task.pruneidxmat_5M;clear('task');
 %task=bip; disp_trait1_name='BIP'; zvec=task.zvec; zvec(~task.defvec_1M) = nan; nvec=task.Neff; pruneidxmat=task.pruneidxmat_1M;clear('task');
-task=bip; disp_trait1_name='BIP'; zvec=task.zvec; zvec(~task.defvec_5M) = nan; nvec=task.Neff; pruneidxmat=task.pruneidxmat_5M;clear('task');
+%task=bip; disp_trait1_name='BIP'; zvec=task.zvec; zvec(~task.defvec_5M) = nan; nvec=task.Neff; pruneidxmat=task.pruneidxmat_5M;clear('task');
+
+%task=bmi; disp_trait1_name='BMI'; zvec=task.zvec; zvec(~task.defvec_1M) = nan; nvec=task.nvec; pruneidxmat=task.pruneidxmat_1M;clear('task');
+%task=height; disp_trait1_name='HEIGHT'; zvec=task.zvec; zvec(~task.defvec_1M) = nan; nvec=task.nvec; pruneidxmat=task.pruneidxmat_1M;clear('task');
+task=scz3; disp_trait1_name='SCZsep17'; zvec=task.zvec; zvec(~task.defvec_1M) = nan; nvec=task.nvec; pruneidxmat=task.pruneidxmat_1M;clear('task');
 
 if 1
     options.total_het = 2 * 1037117.5140529468;  % Total heterozigosity across all SNPs in 1kG phase3
@@ -182,7 +203,7 @@ options.title = sprintf('%s (infinitesimal model)', disp_trait1_name);
 print(figures.tot, sprintf('%s_%i_inf', disp_trait1_name, sum(isfinite(zvec))),'-dpdf')
 set(figures.bin,'PaperOrientation','landscape','PaperPositionMode','auto','PaperType','a3'); % https://se.mathworks.com/help/matlab/ref/matlab.ui.figure-properties.html
 print(figures.bin, sprintf('%s_%i_inf_HL', disp_trait1_name, sum(isfinite(zvec))),'-dpdf')
-
+ 
 options.title = sprintf('%s (null+small+large)', disp_trait1_name);
 [result_mix2_cdf, figures] = UGMG_qq_plot(result_mix2.univariate{1}.params, zvec, Hvec_9m, nvec, pruneidxmat, ref_ld, options, result_mix2_cdf);
 print(figures.tot, sprintf('%s_%i_mix2', disp_trait1_name, sum(isfinite(zvec))),'-dpdf')

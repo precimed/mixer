c595099f9f8eab409503ebfb07ec6a3e9b9fb166
Oleksandr Frei <oleksandr.frei@gmail.com>
2018-08-10 12:20:50 +0200

    bgmg cpp log external messages

--- src/bgmg_matlab.h
+++ src/bgmg_matlab.h
@@ -23,6 +23,7 @@
 
 DLL_PUBLIC const char* bgmg_get_last_error();
 DLL_PUBLIC void bgmg_init_log(const char* file);
+DLL_PUBLIC void bgmg_log_message(const char* message);
 DLL_PUBLIC int64_t bgmg_dispose(int context_id);
 DLL_PUBLIC const char* bgmg_status(int context_id);
 DLL_PUBLIC int64_t bgmg_set_tag_indices(int context_id, int num_snp, int num_tag, int* tag_indices);

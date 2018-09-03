3243b781637378f0cc6c75b732f95b0b236b65d5
Oleksandr Frei <oleksandr.frei@gmail.com>
2018-09-03 10:17:56 +0200

    construct reference from multiple files

--- src/bgmg_main.cc
+++ src/bgmg_main.cc
@@ -121,22 +121,32 @@ void log_header(int argc, char *argv[]) {
 
 struct BgmgOptions {
   std::string bim;
+  std::string bim_chr;
+  std::vector<std::string> bim_files;
+  std::vector<std::string> chr_labels;
   std::string out;
+
 };
 
 void describe_bgmg_options(BgmgOptions& s) {
   LOG << "Options in effect (after applying default setting to non-specified parameters):";
   if (!s.bim.empty()) LOG << "\t--bim " << s.bim << " \\";
+  if (!s.bim.empty()) LOG << "\t--bim-chr " << s.bim_chr << " \\";
   if (!s.out.empty()) LOG << "\t--out " << s.out << " \\";
 }
 
 class BimFile {
 public:
-  BimFile(std::string filename) {
-    LOG << "Reading " << filename << "...";
+  BimFile() {}
+  BimFile(std::string filename) { read(filename); }
+
+  void read(std::string filename) {
+    //LOG << "Reading " << filename << "...";
+
+    const std::string separators = " \t\n\r";
+    std::vector<std::string> tokens;
 
     std::ifstream file(filename, std::ios_base::in | std::ios_base::binary);
-    std::stringstream ss;
     int line_no = 0;
     boost::iostreams::filtering_istream in;
     if (boost::algorithm::ends_with(filename, ".gz")) in.push(boost::iostreams::gzip_decompressor());
@@ -147,9 +157,17 @@ public:
       int chr_label, bp;
       float gp;
       std::string snp, a1, a2;
-      ss.str(str);
-      ss >> chr_label >> snp >> gp >> bp >> a1 >> a2; 
-      if (ss.fail()) {
+
+      boost::trim_if(str, boost::is_any_of(separators));
+      boost::split(tokens, str, boost::is_any_of(separators), boost::token_compress_on);
+      try {
+        chr_label = stoi(tokens[0]);  // must not use stream or boost::lexical_cast (result in some lock contention)	
+        snp = tokens[1];
+        gp = stof(tokens[2]);
+        bp = stoi(tokens[3]);
+        a1 = tokens[4];
+        a2 = tokens[5];
+      } catch (...) {
         std::stringstream error_str;
         error_str << "Error parsing " << filename << ":" << line_no << " ('" << str << "')";
         throw std::invalid_argument(error_str.str());
@@ -162,8 +180,30 @@ public:
       a2_.push_back(a2);
     }
 
-    LOG << "Found " << chr_label_.size() << " variants.\n";
+    LOG << "Found " << chr_label_.size() << " variants in " << filename;
   }
+
+  BimFile(std::vector<std::string> filenames) {
+    LOG << "Construct reference from " << filenames.size() << " files...";
+    std::vector<BimFile> bim_files;
+    bim_files.resize(filenames.size());
+
+#pragma omp parallel for schedule(dynamic)
+    for (int i = 0; i < filenames.size(); i++)
+      bim_files[i].read(filenames[i]);
+
+    for (int i = 0; i < filenames.size(); i++) {
+      chr_label_.insert(chr_label_.end(), bim_files[i].chr_label_.begin(), bim_files[i].chr_label_.end());
+      snp_.insert(snp_.end(), bim_files[i].snp_.begin(), bim_files[i].snp_.end());
+      gp_.insert(gp_.end(), bim_files[i].gp_.begin(), bim_files[i].gp_.end());
+      bp_.insert(bp_.end(), bim_files[i].bp_.begin(), bim_files[i].bp_.end());
+      a1_.insert(a1_.end(), bim_files[i].a1_.begin(), bim_files[i].a1_.end());
+      a2_.insert(a2_.end(), bim_files[i].a2_.begin(), bim_files[i].a2_.end());
+    }
+
+    LOG << "Found " << chr_label_.size() << " variants in total.";
+  }
+
 
 private:
   std::vector<int> chr_label_;
@@ -172,14 +212,35 @@ private:
   std::vector<int> bp_;
   std::vector<std::string> a1_;
   std::vector<std::string> a2_;
-  friend class BimFileParser;
 };
 
 void fix_and_validate(BgmgOptions& bgmg_options, po::variables_map& vm) {
-  // Validate --bim option
-  if (bgmg_options.bim.empty()) throw std::invalid_argument(std::string("ERROR: --bim option must be specified"));
-  if (!boost::filesystem::exists(bgmg_options.bim)) {
-    std::stringstream ss; ss << "ERROR: input file " << bgmg_options.bim << " does not exist";
+  // initialize chr_labels
+  if (bgmg_options.chr_labels.empty()) {
+    for (int i = 1; i <= 22; i++)
+      bgmg_options.chr_labels.push_back(boost::lexical_cast<std::string>(i));
+  }
+
+  // Validate --bim / --bim-chr option
+  if (bgmg_options.bim.empty() && bgmg_options.bim_chr.empty())
+    throw std::invalid_argument(std::string("ERROR: Either --bim or --bim-chr must be specified"));
+
+  if (!bgmg_options.bim_chr.empty()) {
+    if (!boost::contains(bgmg_options.bim_chr, "@"))
+      throw std::invalid_argument(std::string("ERROR: --bim-chr must have @ to indicate location of chr label"));
+
+    for (auto chrlabel : bgmg_options.chr_labels) {
+      bgmg_options.bim_files.push_back(bgmg_options.bim_chr);
+      boost::replace_all(bgmg_options.bim_files.back(), "@", chrlabel);
+    }
+  }
+  else {
+    bgmg_options.bim_files.push_back(bgmg_options.bim);
+  }
+
+  for (auto& bim_file: bgmg_options.bim_files)
+  if (!boost::filesystem::exists(bim_file)) {
+    std::stringstream ss; ss << "ERROR: input file " << bim_file << " does not exist";
     throw std::runtime_error(ss.str());
   }
 
@@ -194,7 +255,9 @@ int main(int argc, char *argv[]) {
     po::options_description po_options("BGMG " VERSION " - Univariate and Bivariate causal mixture models for GWAS");
     po_options.add_options()
       ("help,h", "produce this help message")
-      ("bim", po::value(&bgmg_options.bim), "Full path to .bim file that defines the reference set of SNPs")
+      ("bim", po::value(&bgmg_options.bim), "Path to .bim file that defines the reference set of SNPs")
+      ("bim-chr", po::value(&bgmg_options.bim_chr), "Path to .bim files, split per chromosome. Use @ symbol to indicate location of chromosome label.")
+      ("chr-labels", po::value(&bgmg_options.chr_labels), "List of chromosome labels. Default to 1-22.")
       ("out", po::value(&bgmg_options.out)->default_value("bgmg"),
         "prefix of the output files; "
         "See README.md file for detailed description of file formats.")
@@ -221,7 +284,7 @@ int main(int argc, char *argv[]) {
       fix_and_validate(bgmg_options, vm);
       describe_bgmg_options(bgmg_options);
 
-      BimFile bim_file(bgmg_options.bim);
+      BimFile bim_file(bgmg_options.bim_files);
 
       auto analysis_finished = boost::posix_time::second_clock::local_time();
       LOG << "Analysis finished: " << analysis_finished;

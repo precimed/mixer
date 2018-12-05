712304776f5f24b9d0bd2688af72304c6db59926
interCM <inter.cm@outlook.com>
2018-12-05 23:02:13 +0100

    makeinput.py: create pickled ld files

--- makeinput.py
+++ makeinput.py
@@ -2,216 +2,106 @@ import configparser
 import datetime
 import sys
 import os
-import numpy as np
 import pandas as pd
-from scipy.stats import norm
 from multiprocessing import Pool
 
 
-def process_template_chr(c, template_dir, out_dir):
-    print(f"processing template chr {c}")
-    bim_file = os.path.join(template_dir, f"chr{c}.bim")
-    snp = np.loadtxt(bim_file, usecols=1, dtype=bytes)
-    ind = np.arange(len(snp), dtype='u4')
-    snp_ind_dict = dict(zip(snp, ind))
-    assert len(snp) == len(snp_ind_dict), f"Duplicated variant ids"
-    # print(f"   {len(snp)} variants")
-
-    snp2ind = lambda snp: snp_ind_dict[snp]
-
-    ld_file = os.path.join(template_dir, f"chr{c}.r2.ld.gz")
-    r2_1i, r2_2i, r2 = np.loadtxt(ld_file, skiprows=1, usecols=[2,5,6], 
-        dtype={'names':("r2_1i","r2_2i","r2"),'formats':('u4','u4','f4')},
-        converters={2:snp2ind,5:snp2ind}, unpack=True)
-    # print(f"   {len(r2)} r2 coefficients")
-
-    r2_12i = np.concatenate([r2_1i, r2_2i, ind])
-    r2_21i = np.concatenate([r2_2i, r2_1i, ind])
-    r2 = np.concatenate([r2, r2, np.ones(len(ind), dtype='f4')])
-    i = np.argsort(r2_12i)
-    r2_12i = r2_12i[i]
-    r2_21i = r2_21i[i]
-    r2 = r2[i]
-
-    # ld_bs = LD block size, ld_bs[i] = number of variants in ld with i-th variant from snp (template) array
-    ld_bs = np.searchsorted(r2_12i, ind, side='right')
-    ld_bs[1:] -= ld_bs[:-1]
-
-    frq_file = os.path.join(template_dir, f"chr{c}.maf.frq")
-    si, maf = np.loadtxt(frq_file, skiprows=1, usecols=[1,4], 
-        dtype={'names':("si","maf"),'formats':('u4','f4')},
-        converters={1:snp2ind}, unpack=True)
-    assert len(maf) == len(snp), "Length of maf and snp vectors should be equal"
-    maf = maf[np.argsort(si)] # just to be sure that we have correct order
-    het = 2*maf*(1 - maf)
-
-    snp = snp.astype(str) # convert to unicode in python 3
-    out_file = os.path.join(out_dir, f"template.chr{c}.npz")
-    # May be use numpy.savez_compressed instead of uncompressed savez
-    # r2_21i: [u4] indices of variants in ld blocks corresponding to r2 array, r2 = corr^2(r2_12i, r2_21i)
-    # r2_21i is required to get het and annot indices for s2 and annot_s2 arrays correspondingly
-    np.savez(out_file, snp=snp, ind=ind, het=het, ld_bs=ld_bs, r2_21i=r2_21i, r2=r2)
-    print(f"{out_file} created")
-    return len(snp)
-
-
-def process_sumstats(sumstats_file, sumstats_out_id, out_dir, snp_col, p_col,
-    effect_col, effect_baseline, n_col=None, ncase_col=None, ncontrol_col=None):
-    print(f"Processing sumstats {sumstats_file}")
-    usecols = [snp_col, p_col, effect_col]
-    if (n_col is None) and (not ncase_col is None) and (not ncontrol_col is None):
-        usecols += [ncase_col, ncontrol_col]
-    elif (not n_col is None):
-        usecols.append(n_col)
-    else:
-        raise ValueError(
-            "Either 'n_col' or both 'ncase_col' and 'ncontrol_col' must be not None")
-    df = pd.read_table(sumstats_file, delim_whitespace=True, usecols=usecols,
-        index_col=snp_col)
-    print(f"{len(df)} variants")
-    df = df.loc[np.isfinite(df[p_col]),:]
-    print(f"{len(df)} variants with defined p-value")
-    df = df.loc[df[p_col]>0,:]
-    print(f"{len(df)} variants with p-value > 0")
-    i = df.index.duplicated(keep='first')
-    if i.any():
-        print(f"{i.sum()} duplicated variant ids")
-        print("Only the first row with duplicated id will be retained")
-        df = df.loc[~i,:]
-    positive_effect = df[effect_col] > effect_baseline
-    df["Z"] = norm.ppf(0.5*df[p_col]) # here all Z are < 0
-    df.loc[positive_effect,"Z"] *= -1
-
-    if n_col is None:
-        ssize = 4*df[ncase_col]*df[ncontrol_col]/(df[ncase_col]+df[ncontrol_col])
-    else:
-        ssize = df[n_col]
-    ssize = ssize.values
-    
-    out_file = os.path.join(out_dir, f"sumstats.{sumstats_out_id}.npz")
-    np.savez(out_file, snp=df.index.values, z=df.Z.values, ssize=ssize)
-    print(f"{out_file} created")
-
-
-def process_annot(annot_file, annot_out_id, out_dir, snp_col, annot_col):
-    print(f"Processing annot {annot_file}")
-    df = pd.read_table(annot_file, usecols=[snp_col, annot_col],
-        index_col=snp_col)
-    print(f"{len(df)} variants")
-    i = df.index.duplicated(keep='first')
-    if i.any():
-        print(f"{i.sum()} duplicated variant ids")
-        print("Only the first row with duplicated id will be retained")
-        df = df.loc[~i,:]
-    unique_annot = np.sort(df[annot_col].unique())
-    print(f"{len(unique_annot)} unique annotation categories")
-
-    assert len(unique_annot) < 256, "Maximum 255 annotation categories are supported"
-
-    unique_annot_ind = np.arange(len(unique_annot), dtype='u1')
-    annot_ind_dict = dict(zip(unique_annot, unique_annot_ind))
-    annot = np.array([annot_ind_dict[s] for s in df[annot_col]], dtype='u1')
-
-    out_file = os.path.join(out_dir, f"annot.{annot_out_id}.npz")
-    np.savez(out_file, snp=df.index.values, annot=annot, categories=unique_annot,
-        category_ind=unique_annot_ind)
-    print(f"{out_file} created")
-
-
-def process_qq_annot(qq_annot_file, qq_annot_out_id, out_dir):
+def read_bim(bim_f, verbose=False):
     """
-    Produce npz file with annotations for qq plot. Each SNP in qq
-    plot annotaions can belong to several catigories (in contrast
-    to template annotations used for optimization, which must be
-    unique).
-    qq_annot matrix in the output npz file should have dtype=np.bool
-    Args:
-        qq_annot_file: a file with annotation for qq plot.
-            Must have a header line. 
-            1st column must contain SNP ids.
-            2nd and further columns form a binary matrix indicating
-            whether corresponding SNP belongs to the annotation
-            category (1) or not (0).
-        qq_annot_out_id: string id which will be used to name output
-            npz file (qq_annot.{qq_annot_out_id},npz).
-        out_dir: directory where output file will be placed.
+    Get SNPs with their chromosomes and positions from bim_f.
+    Return:
+        pd.DataFrame(index=snp_id, data=[chr, bp, a1, a2])
     """
-    print(f"Processing qq annot file {qq_annot_file}")
-    df = pd.read_table(qq_annot_file, index_col=0)
-    print(f"{len(df)} variants")
-    print(f"{len(df.columns)} annotation categories")
-    i = df.index.duplicated(keep='first')
-    if i.any():
-        print(f"{i.sum()} duplicated variant ids")
-        print("Only the first row with duplicated id will be retained")
-        df = df.loc[~i,:]
-    out_file = os.path.join(out_dir, f"qq_annot.{qq_annot_out_id}.npz")
-    np.savez(out_file, snp=df.index.values,
-        qq_annot=df.values.astype(np.bool), categories=df.columns)
-    print(f"{out_file} created")
+    bim_df = pd.read_table(bim_f, header=None, usecols=[0,1,3,4,5],
+           names=["CHR","SNP","BP","A1","A2"], index_col="SNP", na_filter=False)
+    if verbose: print(f"{bim_df.shape[0]} SNPs in {bim_f}")
+    return bim_df
 
+
+def read_snp(bim_f, verbose=False):
+    """
+    Get SNPs with their chromosomes and positions from bim_f.
+    Return:
+        pd.Series(index=1..N, data=snp_id)
+    """
+    snp = pd.read_table(bim_f, header=None, names=["SNP"], usecols=[1],
+            na_filter=False, squeeze=True)
+    if verbose: print(f"{snp.size} SNPs in {bim_f}")
+    return snp
+
+
+def read_frq(frq_f, verbose=False):
+    """
+    Get frq values for SNPs.
+    Return:
+        pd.Series(index=snp_id, data=frq)
+    """
+    frq = pd.read_table(frq_f, usecols=["SNP","MAF"], index_col="SNP",
+            na_filter=False, squeeze=True, delim_whitespace=True)
+    if verbose: print(f"{frq.size} SNPs in {frq_f}")
+    return frq
+
+
+def read_ld(ld_f, snp, verbose=False):
+    """
+    Get r2 values from ld_f for SNPs in snp.
+    Return:
+        pd.DataFrame(index=1..N, data=[snp_a_ind, snp_b_ind, r2])
+        where snp_a_ind/snp_b_ind are indices in bim_f of snp_a and snp_b from
+        ld_f correspondingly
+    """
+    snp_ind = {s:i for i,s in enumerate(snp)}
+    snp2ind = lambda s: snp_ind[s]
+    # use 32-bit float type ('f4') for r2 valeus
+    ld_df = pd.read_table(ld_f, na_filter=False, delim_whitespace=True,
+            usecols=["SNP_A","SNP_B","R2"], dtype={"R2":"f4"},
+            converters={"SNP_A":snp2ind,"SNP_B":snp2ind})
+    if verbose: print(f"{ld_df.shape[0]} r2 values in {ld_f}")
+    return ld_df
+
+
+def make_ld_pkl(bim_f, ld_f, ld_pkl_f, verbose=False):
+    snp = read_snp(bim_f, verbose=verbose)
+    ld_df = read_ld(ld_f, snp, verbose=verbose)
+    ld_df.to_pickle(ld_pkl_f)
+    if verbose: print(f"LD information pickled to {ld_pkl_f}")
 
 
 
 if __name__ == "__main__":
     print(f"makeinput.py started at {datetime.datetime.now()}")
-    print("Preparing input data for cmm analysis")
     print(f"Reading config from {sys.argv[1]}")
     cfg = configparser.ConfigParser()
     cfg.read(sys.argv[1])
 
-    make_template = cfg["general"].getboolean("make_template")
-    make_annot = cfg["general"].getboolean("make_annot")
-    make_qq_annot = cfg["general"].getboolean("make_qq_annot")
-    make_sumstats = cfg["general"].getboolean("make_sumstats")
-    out_dir = cfg["general"].get("out_dir")
+    n_proc = cfg["general"].getint("n_proc")
+    verbose = cfg["general"].getboolean("verbose")
 
+    if verbose: print(f"Using {n_proc} processors")
 
-    if make_template:
-        # process template
-        template_dir = cfg["template"].get("template_dir")
-        out_dir = out_dir
-        chr2use = [c for c in cfg["template"].get("chr2use").split()]
-        chr2use.sort(key=lambda x: int(x))
-        n_proc = cfg["template"].getint("n_proc")
+    template_dir = cfg["template"].get("template_dir")
+    chr2use = cfg["template"].get("chr2use").split()
+    bim_prefix = cfg["template"].get("bim_prefix")
+    bim_suffix = cfg["template"].get("bim_suffix")
+    ld_prefix = cfg["template"].get("ld_prefix")
+    ld_suffix = cfg["template"].get("ld_suffix")
+    out_dir = cfg["output"].get("out_dir")
+    if verbose: print(f"Using {len(chr2use)} chromosomes: {', '.join(chr2use)}")
 
-        with Pool(processes=n_proc) as pool:
-            n = sum(pool.starmap(process_template_chr,
-                ((c, template_dir, out_dir) for c in chr2use)))
-        print(f"{n} variants in template")
+    bim_fiels = [os.path.join(template_dir,"".join((bim_prefix,c,bim_suffix)))
+            for c in chr2use]
+    ld_files = [os.path.join(template_dir,"".join((ld_prefix,c,ld_suffix)))
+            for c in chr2use]
+    ld_pkl_files = [os.path.join(out_dir, f"chr{c}.ld.pkl") for c in chr2use]
 
+    for f in (bim_fiels + ld_files):
+        if not os.path.isfile(f):
+            raise ValueError(f"{f} file does not exist")
+    if not os.path.isdir(out_dir):
+        raise ValueError(f"{out_dir} dir does not exist")
 
-    if make_annot:
-        # process annot
-        annot_file = cfg["annot"].get("annot_file")
-        annot_out_id = cfg["annot"].get("annot_out_id")
-        out_dir = out_dir
-        snp_col = cfg["annot"].get("snp_col")
-        annot_col = cfg["annot"].get("annot_col")
-        process_annot(annot_file, annot_out_id, out_dir, snp_col, annot_col)
-
-    if make_qq_annot:
-        # process qq annot
-        qq_annot_file = cfg["qq_annot"].get("qq_annot_file")
-        qq_annot_out_id = cfg["qq_annot"].get("qq_annot_out_id")
-        out_dir = out_dir
-        process_qq_annot(qq_annot_file, qq_annot_out_id, out_dir)
-
-
-    if make_sumstats:
-        #process sumstats
-        sumstats_file = cfg["sumstats"].get("sumstats_file")
-        sumstats_out_id = cfg["sumstats"].get("sumstats_out_id")
-        out_dir = out_dir
-        snp_col = cfg["sumstats"].get("snp_col")
-        p_col = cfg["sumstats"].get("p_col")
-        effect_col = cfg["sumstats"].get("effect_col")
-        effect_baseline = cfg["sumstats"].getfloat("effect_baseline") # e.g. 0 for BETA, 1 for OR
-        ncase_col = cfg["sumstats"].get("ncase_col", fallback=None)
-        ncontrol_col = cfg["sumstats"].get("ncontrol_col", fallback=None)
-        n_col = cfg["sumstats"].get("n_col", fallback=None)
-        process_sumstats(sumstats_file, sumstats_out_id, out_dir, snp_col, p_col,
-            effect_col, effect_baseline, n_col=n_col, ncase_col=ncase_col,
-            ncontrol_col=ncontrol_col)
+    v = [verbose]*len(bim_fiels)
+    with Pool(processes=n_proc) as pool:
+        pool.starmap(make_ld_pkl, zip(bim_fiels,ld_files,ld_pkl_files,v))
 
     print(f"makeinput.py finished at {datetime.datetime.now()}")

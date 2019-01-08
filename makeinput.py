89c776496fc06fa7ce99443f05686fd9043aa208
interCM <inter.cm@outlook.com>
2019-01-08 15:30:10 +0100

    makeinput now saves only r2 values and is able to produce random pruning and random pruning weights

--- makeinput.py
+++ makeinput.py
@@ -1,107 +1,283 @@
 import configparser
+import argparse
 import datetime
 import sys
-import os
+import numpy as np
 import pandas as pd
+from scipy import sparse
 from multiprocessing import Pool
 
 
-def read_bim(bim_f, verbose=False):
-    """
-    Get SNPs with their chromosomes and positions from bim_f.
-    Return:
-        pd.DataFrame(index=snp_id, data=[chr, bp, a1, a2])
-    """
-    bim_df = pd.read_table(bim_f, header=None, usecols=[0,1,3,4,5],
-           names=["CHR","SNP","BP","A1","A2"], index_col="SNP", na_filter=False)
-    if verbose: print(f"{bim_df.shape[0]} SNPs in {bim_f}")
-    return bim_df
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description="Prepare input data for optimization.")
+
+    parser.add_argument("config_file", help="configuration file")
+    parser.add_argument("--ld2npz", action="store_true",
+        help="make LD npz files")
+    parser.add_argument("--rprune", action="store_true",
+        help="make random pruned list of SNPs")
+    parser.add_argument("--rpruneweights", action="store_true",
+        help="estimate SNP weights based on random pruning iterations")
+    
+    return parser.parse_args(args)
 
 
-def read_snp(bim_f, verbose=False):
+
+def ld2npz(ld_f, bim_f, out_npz_f):
     """
-    Get SNPs with their chromosomes and positions from bim_f.
-    Return:
-        pd.Series(index=1..N, data=snp_id)
+    Produces npz file with ld block size, i2 and r2 data taking SNP indices
+    according to the bim_f. All SNP ids from plink ld file must be in bim_f.
+    Args:
+        ld_f: plink ld file
+        bim_f: plink bim file
+        out_npz_f: output npz file name
+    Output:
+        out_npz_f npz file with 3 fields:
+            bs: size of ld block, for SNPs from bim_f
+            i2: index of the second SNP (assuming i1 are ordered)
+            r2: r2 correlation values between i1 and i2
+    Example:
+    i1: 1 1 1 1 2 2 2 3 3 3 3 3 4 4 5 5 5   # len(i1) = 2*len(ld_f) + len(bim_f)
+    bs: 4       3     5         2   3       # len(bs) = len(bim_f)
+    i2: 2 3 5 1 1 3 2 1 2 4 5 3 3 4 1 3 5   # len(i2) = len(i1)
+    r2: x y z 1 a s 1 q w e r 1 t 1 d f 1   # x, y ... from [0,1], len(r2) = len(i1)
     """
-    snp = pd.read_table(bim_f, header=None, names=["SNP"], usecols=[1],
-            na_filter=False, squeeze=True)
-    if verbose: print(f"{snp.size} SNPs in {bim_f}")
-    return snp
+    print(f"Processing {ld_f}")
+    snps = np.loadtxt(bim_f, usecols=1, dtype=bytes)
+
+    ind = np.arange(len(snps), dtype='u4')
+    snp_ind_dict = dict(zip(snps, ind))
+    if len(snps) != len(snp_ind_dict):
+        raise ValueError(f"Duplicated variant ids in {bim_f}. Must be unique.")
+
+    snp2ind = lambda snp: snp_ind_dict[snp]
+
+    #rm ld_file = os.path.join(template_dir, f"chr{c}.r2.ld.gz")
+    i1, i2, r2 = np.loadtxt(ld_f, skiprows=1, usecols=[2,5,6], 
+        dtype={'names':("i1","i2","r2"),'formats':('u4','u4','f4')},
+        converters={2:snp2ind,5:snp2ind}, unpack=True)
+
+    i12 = np.concatenate([i1, i2, ind])
+    i12_sort_idx = np.argsort(i12)
+    i12 = i12[i12_sort_idx]
+    # bs[i] = number of SNPs in ld with i-th variant from bim_f
+    bs = np.searchsorted(i12, ind, side='right')
+    bs[1:] -= bs[:-1]
+
+    i21 = np.concatenate([i2, i1, ind])
+    i21 = i21[i12_sort_idx]
+
+    r2 = np.concatenate([r2, r2, np.ones(len(ind), dtype='f4')])
+    r2 = r2[i12_sort_idx]
+
+    np.savez(out_npz_f,bs=bs, i2=i21, r2=r2)
+    print(f"{out_npz_f} created")
 
 
-def read_frq(frq_f, verbose=False):
+
+def rprune(ld_npz_f, bim_f, ld_thresh, out_f, snps2overlap=None):
     """
-    Get frq values for SNPs.
-    Return:
-        pd.Series(index=snp_id, data=frq)
+    Generate a out_f with SNP ids which survives a sungle iteration of random
+    pruning with ld_thresh.
+    Overlap SNP ids from bim_f with SNP ids from snps2overlap_f and
+    Wrapper for _rprune which works with ld_npz_f produced by ld2npz.
+    Args:
+        ld_npz_f: a file generated with ld2npz
+        bim_f: plink bim file
+        ld_thresh: threshold for r2 values (r2 >= ld_thresh are pruned)
+        out_f: output file
+        snps2overlap: SNP ids to overlap with SNPs from bim_f
+    Output:
+        out_f: a file with SNP ids which survived random pruning
     """
-    frq = pd.read_table(frq_f, usecols=["SNP","MAF"], index_col="SNP",
-            na_filter=False, squeeze=True, delim_whitespace=True)
-    if verbose: print(f"{frq.size} SNPs in {frq_f}")
-    return frq
+    print(f"pruning {ld_npz_f}")
+    ld_csr = _ld_npz2ld_csr(ld_npz_f)
+    snps = pd.read_table(bim_f, sep='\t', header=None, usecols=[1],
+            dtype='str', squeeze=True, engine="c", na_filter=False)
+
+    idx2use = None if snps2overlap is None else snps.isin(snps2overlap)
+
+    survived_idx = _rprune(ld_csr, ld_thresh, idx2use)
+    snps[survived_idx].to_csv(out_f, index=False, header=False)
+    print(f"{out_f} created")
 
 
-def read_ld(ld_f, snp, verbose=False):
+
+def rpruneweights(ld_npz_f, bim_f, ld_thresh, n_iter, out_f, snps2overlap=None):
     """
-    Get r2 values from ld_f for SNPs in snp.
-    Return:
-        pd.DataFrame(index=1..N, data=[snp_a_ind, snp_b_ind, r2])
-        where snp_a_ind/snp_b_ind are indices in bim_f of snp_a and snp_b from
-        ld_f correspondingly
+    Wrapper for _rpruneweights which works with ld_npz_f produced by ld2npz.
     """
-    snp_ind = {s:i for i,s in enumerate(snp)}
-    snp2ind = lambda s: snp_ind[s]
-    # use 32-bit float type ('f4') for r2 valeus
-    ld_df = pd.read_table(ld_f, na_filter=False, delim_whitespace=True,
-            usecols=["SNP_A","SNP_B","R2"], dtype={"R2":"f4"},
-            converters={"SNP_A":snp2ind,"SNP_B":snp2ind})
-    if verbose: print(f"{ld_df.shape[0]} r2 values in {ld_f}")
-    return ld_df
+    print(f"estimating pruning weights for {ld_npz_f} from {n_iter} iterations")
+    ld_csr = _ld_npz2ld_csr(ld_npz_f)
+
+    snps = pd.read_table(bim_f, sep='\t', header=None, usecols=[1],
+            dtype='str', squeeze=True, engine="c", na_filter=False)
+
+    idx2use = None if snps2overlap is None else snps.isin(snps2overlap)
+
+    snp_weights = _rpruneweights(ld_csr, ld_thresh, n_iter, idx2use)
+    pd.Series(snp_weights).to_csv(out_f, index=False, header=False)
+    print(f"{out_f} created")
 
 
-def make_ld_pkl(bim_f, ld_f, ld_pkl_f, verbose=False):
-    snp = read_snp(bim_f, verbose=verbose)
-    ld_df = read_ld(ld_f, snp, verbose=verbose)
-    ld_df.to_pickle(ld_pkl_f)
-    if verbose: print(f"LD information pickled to {ld_pkl_f}")
+
+def _ld_npz2ld_csr(ld_npz_f):
+    """
+    Make scipy.sparse.csr matrix from ld_npz_f produced by ld2npz.
+    """
+    ld_npz = np.load(ld_npz_f)
+    bs = ld_npz.get("bs")
+    i2 = ld_npz.get("i2")
+    r2 = ld_npz.get("r2")
+    i1 = np.repeat(np.arange(bs.size), bs)
+    return sparse.coo_matrix((r2, (i1, i2)), shape=(bs.size, bs.size), dtype='f4').tocsr()
+
+
+
+def _rprune(ld_csr, ld_thresh, idx2use):
+    """
+    Get random pruned indices corresponding to ld_thresh considering only SNPs
+    which are True in idx2use. SNPs which have False in idx2use will not
+    survive random pruning.
+    Args:
+        ld_csr: npz file created with ld2npz
+        ld_thresh: threshold for LD r2 values, r2 >= ld_thresh are pruned
+        idx2use: indices of SNPs to use
+    Output:
+        np.array(len=#(SNPs in ld_npz_f), dtype=bool)
+        Bool array with True values corresponding to variants that survived
+        random pruning, and False values corresponding to non-survivals.
+    """
+    n_snps = ld_csr.shape[0]
+    idx = np.arange(n_snps)
+    np.random.shuffle(idx)
+    if idx2use is None:
+        survived = np.ones(n_snps, dtype='bool')
+    else:
+        survived = np.zeros(n_snps, dtype='bool')
+        survived[idx2use] = True
+    for i in idx:
+        if survived[i]:
+            _, j, r2 = sparse.find(ld_csr[i,:])
+            survived[j[r2>=ld_thresh]] = False
+            survived[i] = True
+    return survived
+
+
+
+def _rpruneweights(ld_csr, ld_thresh, n_iter, idx2use=None):
+    """
+    Make weights induced by random pruning with ld_thresh threshold taking into
+    consideration only SNPs which are True in idx2use. SNPs which are False in
+    idx2use will have the weight = 0.
+    """
+    n_snps = ld_csr.shape[0]
+    rprune_w = np.zeros(n_snps)
+    for i in range(n_iter):
+        survived = _rprune(ld_csr, ld_thresh, idx2use)
+        rprune_w += survived
+    rprune_w /= n_iter
+    return rprune_w
 
 
 
 if __name__ == "__main__":
     print(f"makeinput.py started at {datetime.datetime.now()}")
-    print(f"Reading config from {sys.argv[1]}")
-    cfg = configparser.ConfigParser()
-    cfg.read(sys.argv[1])
+    args = parse_args(sys.argv[1:])
+    print(f"Reading config from {args.config_file}")
+    cfg = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+    cfg.read(args.config_file)
 
-    n_proc = cfg["general"].getint("n_proc")
-    verbose = cfg["general"].getboolean("verbose")
+    n_proc = cfg["misc"].getint("n_proc", fallback=1)
 
-    if verbose: print(f"Using {n_proc} processors")
+    if args.ld2npz:
+        print("Running ld2npz")
+        ld_files = cfg["ld2npz"].get("ld_files").split()
+        bim_files = cfg["ld2npz"].get("bim_files").split()
+        out_npz_files = cfg["ld2npz"].get("out_npz_files").split()
 
-    template_dir = cfg["template"].get("template_dir")
-    chr2use = cfg["template"].get("chr2use").split()
-    bim_prefix = cfg["template"].get("bim_prefix")
-    bim_suffix = cfg["template"].get("bim_suffix")
-    ld_prefix = cfg["template"].get("ld_prefix")
-    ld_suffix = cfg["template"].get("ld_suffix")
-    out_dir = cfg["output"].get("out_dir")
-    if verbose: print(f"Using {len(chr2use)} chromosomes: {', '.join(chr2use)}")
+        if not (len(ld_files) == len(bim_files) == len(out_npz_files)):
+            raise ValueError(
+                f"Number of bim, ld and out_npz files in {args.config_file} must be equal")
+        n_proc = min(n_proc, len(ld_files))
+        print(f"Using {n_proc} cores")
+        with Pool(processes=n_proc) as pool:
+            pool.starmap(ld2npz, zip(ld_files, bim_files, out_npz_files))
 
-    bim_fiels = [os.path.join(template_dir,"".join((bim_prefix,c,bim_suffix)))
-            for c in chr2use]
-    ld_files = [os.path.join(template_dir,"".join((ld_prefix,c,ld_suffix)))
-            for c in chr2use]
-    ld_pkl_files = [os.path.join(out_dir, f"chr{c}.ld.pkl") for c in chr2use]
+    if args.rprune:
+        print("Running rprune")
+        ldnpz_files = cfg["rprune"].get("ldnpz_files").split()
+        bim_files = cfg["rprune"].get("bim_files").split()
+        ld_threshold = cfg["rprune"].getfloat("ld_threshold")
+        out_files = cfg["rprune"].get("out_files").split()
+        snps2overlap_f = cfg["rprune"].get("snps2overlap_f")
 
-    for f in (bim_fiels + ld_files):
-        if not os.path.isfile(f):
-            raise ValueError(f"{f} file does not exist")
-    if not os.path.isdir(out_dir):
-        raise ValueError(f"{out_dir} dir does not exist")
+        if not (len(ldnpz_files) == len(bim_files) == len(out_files)):
+            raise ValueError(
+                f"Number of bim, ldnpz and out files in {args.config_file} must be equal")
+        
+        if snps2overlap_f is None:
+            snps2overlap = None
+        else:
+            print(f"reading snps2overlap from {snps2overlap_f}")
+            snps2overlap = pd.read_table(snps2overlap_f, header=None, usecols=[0],
+                    dtype='str', squeeze=True, engine="c", na_filter=False)
 
-    v = [verbose]*len(bim_fiels)
-    with Pool(processes=n_proc) as pool:
-        pool.starmap(make_ld_pkl, zip(bim_fiels,ld_files,ld_pkl_files,v))
+        for ldnpz_f, bim_f, out_f in zip(ldnpz_files, bim_files, out_files):
+            rprune(ldnpz_f, bim_f, ld_threshold, out_f, snps2overlap)
+
+    if args.rpruneweights:
+        print("Running rpruneweights")
+        ldnpz_files = cfg["rpruneweights"].get("ldnpz_files").split()
+        bim_files = cfg["rpruneweights"].get("bim_files").split()
+        ld_threshold = cfg["rpruneweights"].getfloat("ld_threshold")
+        n_iter = cfg["rpruneweights"].getint("n_iter")
+        out_files = cfg["rpruneweights"].get("out_files").split()
+        snps2overlap_f = cfg["rpruneweights"].get("snps2overlap_f")
+
+        if not (len(ldnpz_files) == len(bim_files) == len(out_files)):
+            raise ValueError(
+                f"Number of bim, ldnpz and out files in {args.config_file} must be equal")
+        
+        if snps2overlap_f is None:
+            snps2overlap = None
+        else:
+            print(f"reading snps2overlap from {snps2overlap_f}")
+            snps2overlap = pd.read_table(snps2overlap_f, header=None, usecols=[0],
+                    dtype='str', squeeze=True, engine="c", na_filter=False)
+
+        for ldnpz_f, bim_f, out_f in zip(ldnpz_files, bim_files, out_files):
+            rpruneweights(ldnpz_f, bim_f, ld_threshold, n_iter, out_f, snps2overlap)
+
+
+
+
+
+    # template_dir = cfg["template"].get("template_dir")
+    # chr2use = cfg["template"].get("chr2use").split()
+    # bim_prefix = cfg["template"].get("bim_prefix")
+    # bim_suffix = cfg["template"].get("bim_suffix")
+    # ld_prefix = cfg["template"].get("ld_prefix")
+    # ld_suffix = cfg["template"].get("ld_suffix")
+    # out_dir = cfg["output"].get("out_dir")
+    # if verbose: print(f"Using {len(chr2use)} chromosomes: {', '.join(chr2use)}")
+
+    # bim_fiels = [os.path.join(template_dir,"".join((bim_prefix,c,bim_suffix)))
+    #         for c in chr2use]
+    # ld_files = [os.path.join(template_dir,"".join((ld_prefix,c,ld_suffix)))
+    #         for c in chr2use]
+    # ld_pkl_files = [os.path.join(out_dir, f"chr{c}.ld.pkl") for c in chr2use]
+
+    # for f in (bim_fiels + ld_files):
+    #     if not os.path.isfile(f):
+    #         raise ValueError(f"{f} file does not exist")
+    # if not os.path.isdir(out_dir):
+    #     raise ValueError(f"{out_dir} dir does not exist")
+
+    # v = [verbose]*len(bim_fiels)
+    # with Pool(processes=n_proc) as pool:
+    #     pool.starmap(make_ld_pkl, zip(bim_fiels,ld_files,ld_pkl_files,v))
 
     print(f"makeinput.py finished at {datetime.datetime.now()}")

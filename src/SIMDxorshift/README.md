ff42caa18b8c744ba1831c1e43817fcd7161f44b
Oleksandr Frei <oleksandr.frei@gmail.com>
2019-10-12 14:59:45 +0200

    lemire/SIMDxorshift

---
+++ src/SIMDxorshift/README.md
@@ -0,0 +1,79 @@
+# SIMDxorshift
+
+
+ Xorshift are a family of pseudo-random number generators (RNG) invented by
+ Marsaglia.
+
+ https://en.wikipedia.org/wiki/Xorshift
+
+
+We present   a vectorized version of xorshift128+, a popular random-number generator
+part of this family. It is written in C. The implementation uses Intel's SIMD
+instructions and is based on Vigna's original (pure C) implementation.
+
+As a random number generator, xorshift128+ is not very strong. [It fails statistical
+tests such as BigCrush](https://lemire.me/blog/2017/09/08/the-xorshift128-random-number-generator-fails-bigcrush/). It should never be
+used alone in applications where the quality of the random numbers matters a great
+deal. However, when you just want fast and "good enough" random numbers, it should
+do well.
+
+Since speed is the primary benefit of xorshift128+, then it is tempting to accelerate
+it further using vector instructions.
+
+## Prerequisite
+
+You should have a recent Intel processor (Haswell or better). If you bought your
+PC before everyone on Earth had a smartphone, it is probably too old a PC. Please
+upgrade.
+
+Your compiler supports C99, right? C99 stands for 1999. That was almost 20 years ago.
+
+## Code sample
+
+```C
+#include "simdxorshift128plus.h"
+
+// create a new key
+avx_xorshift128plus_key_t mykey;
+avx_xorshift128plus_init(324,4444,&mykey); // values 324, 4444 are arbitrary, must be non-zero
+
+// generate 32 random bytes, do this as many times as you want
+__m256i randomstuff =  avx_xorshift128plus(&mykey);
+```
+
+## Usage 
+
+```bash
+$ make
+$ ./fillarray
+Generating 5000 32-bit random numbers
+Time reported in number of cycles per array element.
+We store values to an array of size = 19 kB.
+
+We just generate the random numbers:
+populateRandom_xorshift128plus(prec, size):  3.63 cycles per operation
+populateRandom_avx_xorshift128plus(prec, size):  2.21 cycles per operation
+populateRandom_avx_xorshift128plus_two(prec, size):  1.88 cycles per operation
+populateRandom_avx512_xorshift128plus_two(prec, size):  1.47 cycles per operation
+
+```
+
+(Tests on a Skylake-X processor.)
+
+
+## Shallow analysis
+
+SIMD random-number generation is something like twice as fast as plain C random number
+generation. However on algorithms such as random shuffling, the benefits of faster random number generation
+are lesser because other bottlenecks arise. 
+
+For the most part however, the application of SIMD instructions for random number generation is "free"
+if the CPU supports it.
+
+## Related work
+
+* [Vectorized version of the PCG random number generator](https://github.com/lemire/simdpcg)
+
+## Reference
+
+Vigna's xorshift128+ implementation http://xorshift.di.unimi.it/xorshift128plus.c
---
+++ src/SIMDxorshift/simdaesdragontamer.h
@@ -0,0 +1,85 @@
+/***
+* Code from Percy "Dragontamer" Tiglao
+***/
+
+#ifndef AESDRAGONTAMER_H
+#define AESDRAGONTAMER_H
+#if defined( __AES__) && defined(__AVX__) 
+
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <immintrin.h>
+
+typedef struct {
+  __m128i state;
+  __m128i increment;
+} aesdragontamer_state;
+
+
+static inline __m256i aesdragontamer_r(aesdragontamer_state *state) {
+  state->state = _mm_add_epi64(state->state, state->increment);
+  __m128i penultimate = _mm_aesenc_si128(state->state, state->increment); 
+  __m128i penultimate1 = _mm_aesenc_si128(penultimate, state->increment); 
+  __m128i penultimate2 = _mm_aesdec_si128(penultimate, state->increment); 
+  return _mm256_insertf128_si256(_mm256_castsi128_si256(penultimate2), penultimate1, 1);
+}
+
+
+static inline void aesdragontamer_seed_r(uint64_t seed1, uint64_t seed2, aesdragontamer_state *state) {
+  state->increment = _mm_set_epi8(0x2f, 0x2b, 0x29, 0x25, 0x1f, 0x1d, 0x17, 0x13, 
+		0x11, 0x0D, 0x0B, 0x07, 0x05, 0x03, 0x02, 0x01);
+  state->state = _mm_set_epi64x(seed1, seed2);
+}
+
+
+
+static aesdragontamer_state g_aesdragontamer_state;
+
+static inline void aesdragontamer_seed(uint64_t seed1, uint64_t seed2) {
+  aesdragontamer_seed_r(seed1, seed2, &g_aesdragontamer_state);
+}
+
+static inline __m256i aesdragontamer() { return aesdragontamer_r(&g_aesdragontamer_state); }
+
+
+
+
+static __m256i avx_dragonrandombound_epu32(__m256i randomvals, __m256i upperbound) {
+	/* four values */
+	__m256i evenparts = _mm256_srli_epi64(
+			_mm256_mul_epu32(randomvals, upperbound), 32);
+	/* four other values */
+	__m256i oddparts = _mm256_mul_epu32(_mm256_srli_epi64(randomvals, 32),
+			_mm256_srli_epi64(upperbound, 32));
+	/* note:shift could be replaced by shuffle */
+	/* need to blend the eight values */
+	return _mm256_blend_epi32(evenparts, oddparts, 0b10101010);
+}
+
+void aesdragontamer_shuffle32(aesdragontamer_state *key,
+		uint32_t *storage, uint32_t size) {
+	uint32_t i;
+	uint32_t randomsource[8];
+	__m256i interval = _mm256_setr_epi32(size, size - 1, size - 2, size - 3,
+			size - 4, size - 5, size - 6, size - 7);
+	__m256i R = avx_dragonrandombound_epu32(aesdragontamer_r(key), interval);
+	_mm256_storeu_si256((__m256i *) randomsource, R);
+	__m256i vec8 = _mm256_set1_epi32(8);
+	for (i = size; i > 1;) {
+		for (int j = 0; j < 8; ++j) {
+			uint32_t nextpos = randomsource[j];
+			int tmp = storage[i - 1]; // likely in cache
+			int val = storage[nextpos]; // could be costly
+			storage[i - 1] = val;
+			storage[nextpos] = tmp; // you might have to read this store later
+			i--;
+		}
+		interval = _mm256_sub_epi32(interval, vec8);
+		R = avx_dragonrandombound_epu32(aesdragontamer_r(key), interval);
+		_mm256_storeu_si256((__m256i *) randomsource, R);
+	}
+}
+
+#endif // __AES__
+#endif

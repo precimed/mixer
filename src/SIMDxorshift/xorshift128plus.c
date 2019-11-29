870a9d0d47b0cf70fcd92986ebb48e04f0261584
Oleksandr Frei <oleksandr.frei@gmail.com>
2019-11-29 13:36:08 +0100

    make .c files comply with extern C

--- src/SIMDxorshift/xorshift128plus.c
+++ src/SIMDxorshift/xorshift128plus.c
@@ -14,8 +14,10 @@ void xorshift128plus_jump(xorshift128plus_key_t * key) {
     static const uint64_t JUMP[] = { 0x8a5cd789635d2dff, 0x121fd2155c472f96 };
     uint64_t s0 = 0;
     uint64_t s1 = 0;
-    for(unsigned int i = 0; i < sizeof (JUMP) / sizeof (*JUMP); i++)
-        for(int b = 0; b < 64; b++) {
+    unsigned int i;
+    int b;
+    for(i = 0; i < sizeof (JUMP) / sizeof (*JUMP); i++)
+        for(b = 0; b < 64; b++) {
             if (JUMP[i] & 1ULL << b) {
                 s0 ^= key->part1;
                 s1 ^= key->part2;
